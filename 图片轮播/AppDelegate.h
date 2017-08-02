fb567d08982c51d28500e661455cb193b41c403a
lmswh <1293445195@qq.com>
2017-08-02 08:16:31 +0800

    Initial Commit

---
+++ 图片轮播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片轮播
+//
+//  Created by 风鹰 on 2017/8/2.
+//  Copyright © 2017年 风鹰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片轮播/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图片轮播
+//
+//  Created by 风鹰 on 2017/8/2.
+//  Copyright © 2017年 风鹰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
